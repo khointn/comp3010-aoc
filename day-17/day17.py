dde7c6abdeb691325f2a28099e4756f0a94b8edc
KhoiNTN <20khoi.ntn@vinuni.edu.vn>
2022-05-31 16:42:05 +0700

    add day 17

---
+++ day-17/day17.py
@@ -0,0 +1,33 @@
+### day 17
+
+from itertools import combinations
+
+
+def combos_for_size(containers, size):
+    return sum(
+        1
+        for count in range(len(containers))
+        for c in combinations(containers, count + 1)
+        if sum(c) == size)
+
+
+def combos_for_min_size(containers, size):
+    min_count = next(
+        count
+        for count in range(len(containers))
+        for c in combinations(containers, count + 1)
+        if sum(c) == size)
+    return sum(
+        1
+        for c in combinations(containers, min_count + 1)
+        if sum(c) == size)
+
+
+if __name__ == '__main__':
+
+    filename = r'C:\Users\nguye\comp3010-aoc\day-17\day17input.txt'
+    with open(filename) as inputfile:
+        containers = [int(l) for l in inputfile if l.strip()]
+
+    print('Part 1:', combos_for_size(containers, 150))
+    print('Part 2:', combos_for_min_size(containers, 150))
